aa72f3f3337a7578b53bd7f640d2835ed53a04d3
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-11-03 15:44:52 -0300

    centralized the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-fit mx-auto py-8`}
       >
         {children}
       </body>
