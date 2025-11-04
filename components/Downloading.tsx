a4992b20094399ce303c2a2de21e23ec31e5554d
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-11-04 15:34:22 -0300

    create loading animation to button

---
+++ components/Downloading.tsx
@@ -0,0 +1,8 @@
+import './Downloading.scss'
+export default function () {
+  return (
+    <div className="dots" aria-hidden="true">
+      <span></span><span></span><span></span>
+    </div>
+  )
+}
